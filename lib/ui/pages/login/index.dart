755d487f6e6103c041e4e5bebb2ab86194496c11
faqih <faqihsidj@gmail.com>
2022-04-03 15:25:03 +0700

    update compile SDK version

--- lib/ui/pages/login/index.dart
+++ lib/ui/pages/login/index.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text("Login Page"),
+      ),
+    );
   }
 }
